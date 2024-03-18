3d7e74d12d1401be2a57390f19a728e2ead56352
송준섭 <127940281+sjs9668@users.noreply.github.com>
2024-03-18 19:07:06 +0900

    Add files via upload

---
+++ seminar1week.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+//int sum(int a,int b){
+	//cout << a + b << endl;
+	//return 0;
+//}
+int main(){
+	//1번
+	//cout << "HELLO WORLD" << endl;
+
+	//2번
+	//int n = 0;
+	//cin >> n;
+	//for (int i = 0; i < n; i++){
+		//for (int j = 0; j <= i; j++){
+			//cout << "*";
+		//}
+		//cout << endl;
+	//}
+
+	//3번
+	//int a, b;
+	//cin >> a >> b;
+
+	//cout << a + b << endl;
+	//cout << a - b << endl;
+	//cout << a * b << endl;
+	//cout << a / b << endl;
+
+	
+	//4번
+	//int a, b;
+	//cin >> a >> b;
+	//sum(a, b);
+
+
+	
+}
